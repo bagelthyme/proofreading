d4974aaa4621946130401c9e08e13ec646d0a758
TwistedBagel <darthmars97@gmail.com>
2020-03-28 17:55:40 -0600

    add scroll to top button

--- scripts/nav.js
+++ scripts/nav.js
@@ -5,3 +5,7 @@ document.querySelector("#aboutNav").onclick = function() {
 document.querySelector("#faqNav").onclick = function() {
   document.querySelector("#frequently").scrollIntoView({ behavior: 'smooth' });
 }
+
+document.querySelector("#scrollUp").onclick = function() {
+  document.querySelector("body").scrollIntoView( {behavior: 'smooth' });
+}
