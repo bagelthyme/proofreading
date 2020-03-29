08c79ef95f23ca4b48e0b4a2cea3d81ae46a4443
TwistedBagel <darthmars97@gmail.com>
2020-03-28 22:42:51 -0600

    contact.php

---
+++ contact.php
@@ -0,0 +1,6 @@
+<?php
+echo "<p>making sure the form works so that it can be plugged up to email once ready to go truly live";
+foreach ($_POST as $k => $v) {
+    echo "<p>{$k} :: {$v}</p>";
+}
+?>
