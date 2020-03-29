35d9c1a0138cb0819d9c73e9835b6da9381fad9d
TwistedBagel <darthmars97@gmail.com>
2020-03-28 20:57:01 -0600

    work on mobile

--- scripts/nav.js
+++ scripts/nav.js
@@ -9,3 +9,22 @@ document.querySelector("#faqNav").onclick = function() {
 document.querySelector("#scrollUp").onclick = function() {
   document.querySelector("body").scrollIntoView( {behavior: 'smooth' });
 }
+
+var $burger = $('#menuToggle');
+var $logo = $('#logo');
+var $nav = $('nav ul');
+var hidden = true;
+
+$burger.on('click', () => {
+    if (hidden) {
+        $logo.css('display', 'none');
+        $nav.css('overflow', 'unset');
+        $nav.css('visibility', 'unset');
+        hidden = false;
+    } else {
+        $logo.css('display', 'flex');
+        $nav.css('overflow', 'hidden');
+        $nav.css('visibility', 'hidden');
+        hidden = true;
+    }
+})
