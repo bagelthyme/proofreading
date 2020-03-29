414c03c75829bc3c0885f492ebb1ea2196a2556f
TwistedBagel <darthmars97@gmail.com>
2020-03-28 22:01:54 -0600

    add new mobile view

--- scripts/nav.js
+++ scripts/nav.js
@@ -13,6 +13,7 @@ document.querySelector("#scrollUp").onclick = function() {
 var $burger = $('#menuToggle');
 var $logo = $('#logo');
 var $nav = $('nav ul');
+var $phone = $('nav p');
 var hidden = true;
 
 $burger.on('click', () => {
@@ -20,11 +21,15 @@ $burger.on('click', () => {
         $logo.css('display', 'none');
         $nav.css('overflow', 'unset');
         $nav.css('visibility', 'unset');
+        $phone.css('overflow', 'unset');
+        $phone.css('visibility', 'unset');
         hidden = false;
     } else {
-        $logo.css('display', 'flex');
+        $logo.css('display', 'unset');
         $nav.css('overflow', 'hidden');
         $nav.css('visibility', 'hidden');
+        $phone.css('overflow', 'hidden');
+        $phone.css('visibility', 'hidden');
         hidden = true;
     }
 })
