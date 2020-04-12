9c258dcfb25e3d9ece28c1cd7780f1eb93e3854f
TwistedBagel <darthmars97@gmail.com>
2020-04-12 17:52:36 -0600

    get file upload working and make sure uploaded files aren't uploaded to git

--- checkout.php
+++ checkout.php
@@ -1,3 +1,25 @@
+<?php
+  $targetDir = 'uploads/';
+  $targetFile = $targetDir . basename($_FILES['fileUpload']["name"]);
+  $file_type = $_FILES['fileUpload']['type'];
+  $file_ext = strtolower(end(explode('.',$_FILES['fileUpload']['name'])));
+  $uploadOk = 1;
+
+  $extensions= array("pdf", "doc", "docx");
+      
+  if(in_array($file_ext,$extensions)=== false){
+      $errors[]="extension not allowed.";
+  }
+  
+  if(empty($errors)==true){
+      move_uploaded_file($_FILES['fileUpload']["tmp_name"], $targetFile);
+      echo "Success";
+  }else{
+      print_r($errors);
+  }
+
+?>
+
 <!DOCTYPE html>
 <html></<!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -9,24 +31,24 @@
       <div class="grid-item confirmation-container">
 
         <div class="actionEditing">
-          <form id="confirmationForm">
+          <form id="confirmationForm" method='post' action='' enctype='multipart/form-data'>
             <label>
               <label for="pages">Upload your file here...</label><br>
+              <input type='file' name='fileUpload'>
               <input type="text" id="fname" name="fname" value="0"><br>
               <textarea></textarea><br>
+              <input type="submit">
             </label>
           </form>
         </div>
 
       <div>
-        <div class="confirmationEditing">Totla price:</div>
+        <div class="confirmationEditing">Total price:</div>
         <button id="checkoutButton">Checkout</button>
       </div>
     </div>
 
-
-
-      <?php include 'footer.php';?>
+    <?php include 'footer.php';?>
     </div>
 
   </body>
